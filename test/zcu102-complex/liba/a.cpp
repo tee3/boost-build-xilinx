739e821e6371d5f7327b41f0b60b48f7e45d4657
Thomas Brown <tabsoftwareconsulting@gmail.com>
2019-07-06 00:49:54 -0400

    Add copyright information to all files.

--- test/zcu102-complex/liba/a.cpp
+++ test/zcu102-complex/liba/a.cpp
@@ -1,1 +1,6 @@
+// Copyright 2019 Thomas Brown
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 void a () {}
